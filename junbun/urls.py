052fa95b4d9175f5305a53dba0a179c993ddf966
Toma Ryu <tma951070@gmail.com>
2025-02-18 21:57:18 +0900

    Fix fucking program

--- junbun/urls.py
+++ junbun/urls.py
@@ -32,3 +32,4 @@ urlpatterns = [
 # メディアファイルを提供するための設定
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
